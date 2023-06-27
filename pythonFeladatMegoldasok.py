bfc287d1ab9f4a2cec5c9e24ef25f0e9f64d606c
borossandor27 <boros.sandor@dszcberegszaszi.hu>
2023-06-27 10:31:28 +0200

    f06

--- pythonFeladatMegoldasok.py
+++ pythonFeladatMegoldasok.py
@@ -5,7 +5,7 @@ Az INFORMATIKA ÉS TÁVKÖZLÉS ágazathoz tartozó
 
 Ágazati alapvizsga követelményeire felkészítő egyszerű feladatok
 """
-import os
+import os, math
 
 
 def f01():
@@ -46,11 +46,41 @@ def f03b():
     print(f"\nA {parosSzam01} fele: {parosSzam01 / 2}")
 
 
+def f04():
+    # Kérjünk be a felhasználótól egy valós számot, majd adjuk meg a harmadát.
+    os.system("cls")
+    valosSzam = float(input("Kérem adjon meg egy valós értéket: "))
+    print(f"A {valosSzam} szám haramada: {(valosSzam/3):.3f}")
+
+
+def f05():
+    # Kérjük be a felhasználótól egy kör alakú medence átmérőjét és mélységét, majd adjuk meg, hogy hány köbméter víz fér bele.
+    os.system("cls")
+    atmero = float(input("Kérem adja meg a medence átmérőjét méterben: "))
+    magassag = float(input("Kérem adja meg a medence magasságát méterben: "))
+    terfogat = math.pi * math.pow(atmero / 2, 2) * magassag
+    print(
+        f"A {atmero} m átmérőjű és {magassag} m magasságú medence térfogta: {(terfogat):.3f} m"
+        + "\u00b3"
+    )
+
+
+def f06():
+    # Oldja meg az 'ax + b = 0'  alakú elsőfokú egyenletet! 
+    os.system("cls")
+    a = float(input("Kérem adja meg az 'ax + b = 0' egyenlet 'a' értékét: "))
+    b = float(input("Kérem adja meg az 'ax + b = 0' egyenlet  'b' értékét: "))
+    print(f"\nx értéke: {(-b/a):.4f}")
+
+
 def main():
     # f01()
     # f02()
     # f03()
-    f03b()
+    # f03b()
+    # f04()
+    # f05()
+    f06()
 
 
 main()
